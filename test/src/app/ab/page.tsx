4f412687a64d3055106252e15f1dc78f268380c8
Avi Srivastava <srivastavaavi26@gmail.com>
2025-06-26 12:21:21 +0530

    clean init

---
+++ test/src/app/ab/page.tsx
@@ -0,0 +1,11 @@
+import Footer from '@/components/Footer'
+import React from 'react'
+
+function page() {
+  return (
+    <div>page</div>
+    <Footer
+  )
+}
+
+export default page
---
+++ test/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/Footer'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <div>page</div>
+    <p>hii hello</p>
+    <Footer />
+    </>
+  )
+}
+
+export default page
